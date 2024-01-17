1e16d516594fb32c5e6909c483a77f76ab8eb2c0
Shawn <chenhsiangchou@gmail.com>
2024-01-17 23:48:05 +0800

    只加了settings page右下角一個add button

--- lib/page/settings_page.dart
+++ lib/page/settings_page.dart
@@ -1,13 +1,39 @@
 import 'package:flutter/material.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   const SettingsPage({super.key});
+
+  @override
+  _SettingsPage createState() => _SettingsPage();
+}
+
+class _SettingsPage extends State<SettingsPage> {
+  bool _showPlus = true;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-      ),
+        appBar: AppBar(
+        ),
+        body: Stack(
+          children: [
+            Positioned(
+              bottom: 20.0, // Adjust the values as per your requirements
+              right: 20.0,
+              child: Visibility(
+                visible: _showPlus,
+                child: FloatingActionButton(
+                  onPressed: () {
+                    setState(() {
+                      _showPlus = false;
+                    });
+                  },
+                  child: const Icon(Icons.add),
+                ),
+              ),
+            ),
+          ],
+        ),
     );
   }
 }
