a57f14e73418f136cfef48dcf9969127e034f6ca
Shawn <chenhsiangchou@gmail.com>
2024-04-24 21:22:40 +0800

    import issue upgrade lib version

--- android/app/src/main/kotlin/com/example/robot_living/AlarmReceiver.java
+++ android/app/src/main/kotlin/com/example/robot_living/AlarmReceiver.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 
+import androidx.core.app.NotificationCompat;
+
 public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
